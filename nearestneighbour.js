c66f7530e9b090f42e2f5f4655808cee1d52b73b
Anton Panchishin <Anton.Panchishin@intelliware.com>
2015-09-01 09:45:14 -0400

    refactor options out

--- nearestneighbour.js
+++ nearestneighbour.js
@@ -54,18 +54,35 @@ function distances( subject , objects , options ) {
   return results;
 }
 
-module.exports = function(subject, objects, options) {
+module.exports = function( options ) {
   options = options || {}
   options.filter = options.filter || function() { return true }
   options.distanceFunction = options.distanceFunction || defaultDistanceFunction
+  options.objects = options.objects || []
 
-  var sortMap = distances( subject , objects.filter( options.filter ) , options )
-    .sort( function( a , b ) { return a.dist - b.dist } )
-    .map( function( item ) { return item.object } )
+  return {
+    setObjects : function setObjects( newObjects ) {
+      options.objects = newObjects
+    },
+    addObject : function addObject( object ) {
+      options.objects.push(object)
+    },
+    config : function config( config ) {
+      config = config || {}
+      options.filter = config.filter || options.filter
+      options.distanceFunction = config.distanceFunction || options.distanceFunction
+      options.objects = config.objects || options.objects
+    },
+    nearest : function nearest( subject ) {
+      var sortMap = distances( subject , options.objects.filter( options.filter ) , options )
+        .sort( function( a , b ) { return a.dist - b.dist } )
+        .map( function( item ) { return item.object } )
 
-  if ( options.number ) {
-    return sortMap.slice(0, options.number )
-  } else {
-    return sortMap
+      if ( options.number ) {
+        return sortMap.slice(0, options.number )
+      } else {
+        return sortMap
+      }
+    }
   }
 }
--- test/basic.js
+++ test/basic.js
@@ -6,67 +6,63 @@ module.exports = {
 	},
 
 	'nearestNeighbour constructor creates object' : function(beforeExit, assert) {
-		var nearestNeighbour = nearestNeighbourConstructor( { } , [ ] );
+		var nearestNeighbour = nearestNeighbourConstructor( )
 		assert.equal('object' , typeof nearestNeighbour )
 	},
 
 
 	'complete successfully for no items' : function(beforeExit, assert) {
-		var nearestNeighbour = nearestNeighbourConstructor( {} , [ ] );
-		assert.equal( 0 , nearestNeighbour.length )
+		var nearestNeighbour = nearestNeighbourConstructor( )
+		assert.equal( 0 , nearestNeighbour.nearest().length )
 	},
 
 	'complete successfully for 1 item' : function(beforeExit, assert) {
 		var nearestNeighbour = nearestNeighbourConstructor( 
-			{ a : 1 },
-			[ { a : 2 } ]
+			{ objects : [ { a : 2 } ] }
 		)
-		assert.equal( 2 , nearestNeighbour[0].a )
+		assert.equal( 2 , nearestNeighbour.nearest({a:1})[0].a )
 	},
 
 	'complete successfully for 2 items' : function(beforeExit, assert) {
-		var nearestNeighbour = nearestNeighbourConstructor( 
-			{ a : 1 },
-			[ { a : 2 } , { a : -10 } ]
+		var nearestNeighbour = nearestNeighbourConstructor(
+			{ objects : [ { a : 2 } , { a : -10 } ] }
 		)
-		assert.equal( 2 , nearestNeighbour[0].a )
-		assert.equal( -10 , nearestNeighbour[1].a )
+		assert.equal( 2 , nearestNeighbour.nearest({ a : 1 })[0].a )
+		assert.equal( -10 , nearestNeighbour.nearest({ a : 1 })[1].a )
 	},
 
 	'complete successfully for 2 items backwards' : function(beforeExit, assert) {
 		var nearestNeighbour = nearestNeighbourConstructor( 
-			{ a : 1 },
-			[ { a : 10 } , { a : -2 } ]
+			{ objects : [ { a : 10 } , { a : -2 } ] }
 		)
-		assert.equal( -2 , nearestNeighbour[0].a )
-		assert.equal( 10 , nearestNeighbour[1].a )
+		assert.equal( -2 , nearestNeighbour.nearest({ a : 1 })[0].a )
+		assert.equal( 10 , nearestNeighbour.nearest({ a : 1 })[1].a )
 	},
 
 
 	'complete successfully for function override' : function(beforeExit, assert) {
 		var nearestNeighbour = nearestNeighbourConstructor( 
-			{ a : 1 },
-			[ { a : 10 } , { a : -3 } ]
+			{ objects : [ { a : 10 } , { a : -3 } ] }
 		)
-		assert.equal( -3 , nearestNeighbour[0].a )
-		assert.equal( 10 , nearestNeighbour[1].a )
+		assert.equal( -3 , nearestNeighbour.nearest({ a : 1 })[0].a )
+		assert.equal( 10 , nearestNeighbour.nearest({ a : 1 })[1].a )
 
 		var nearestNeighbour = nearestNeighbourConstructor( 
-			{ a : 1 },
-			[ { a : 10 } , { a : -3 } ] ,
-			{ distanceFunction : function(o1,o2) { return Math.abs( o1.a - (o2.a % 10 ) ) }}
+			{
+				objects : [ { a : 10 } , { a : -3 } ] ,
+				distanceFunction : function(o1,o2) { return Math.abs( o1.a - (o2.a % 10 ) ) }
+			}
 		)
-		assert.equal( 10 , nearestNeighbour[0].a )
-		assert.equal( -3 , nearestNeighbour[1].a )
+		assert.equal( 10 , nearestNeighbour.nearest({ a : 1 })[0].a )
+		assert.equal( -3 , nearestNeighbour.nearest({ a : 1 })[1].a )
 	},
 
 	'complete successfully for 2 dimentions' : function(beforeExit, assert) {
 		var nearestNeighbour = nearestNeighbourConstructor( 
-			{ a : 1  , b : 1},
-			[ { a : 2 , b : 100} , { a : -10 , b : -10 } ]
+			{ objects : [ { a : 2 , b : 100} , { a : -10 , b : -10 } ] }
 		)
-		assert.equal( -10 , nearestNeighbour[0].a )
-		assert.equal( 2 , nearestNeighbour[1].a )
+		assert.equal( -10 , nearestNeighbour.nearest({ a : 1, b : 1 })[0].a )
+		assert.equal( 2 , nearestNeighbour.nearest({ a : 1, b : 1 })[1].a )
 	}
 
 }
